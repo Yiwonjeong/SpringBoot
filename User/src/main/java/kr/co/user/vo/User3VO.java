0edbe56f8d9cd9f88eb25b59f0407ae44b3f2a7f
Yiwonjeong <near_and_dear_studio@naver.com>
2023-01-10 18:57:34 +0900

    2023/01/10

---
+++ User/src/main/java/kr/co/user/vo/User3VO.java
@@ -0,0 +1,20 @@
+package kr.co.user.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class User3VO {
+
+	private String uid;
+	private String name;
+	private String hp;
+	private int age;
+}
